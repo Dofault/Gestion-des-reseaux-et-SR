d59803dda41c1c05c2b4853ee444eaffc50c08c7
hadrien007 <97252846+hadrien007@users.noreply.github.com>
2023-09-21 09:24:08 +0200

    Update Projet.py

--- Projet.py
+++ Projet.py
@@ -8,24 +8,44 @@ def ipEntered():
             return ipaddress.ip_address(ip)
         except ValueError:
             print("L'ip n'est pas valideS")
-ip=ipEntered()            
+          
 
 
-print ('Veuillez entrez un masque ')
-masque = input()
 
 
-print ('Voulez-vous une découpe en sous-réseaux ? (oui/non) ')
-reponse = input()
 
-if reponse=="oui":
-    print(ip)
-    print(masque)
-    
-    print(sr)
+while True:
+    print("Menu:")
+    print("1. Calculer l'adresse de réseau, l'adresse de broadcast et l'adresse du sous-réseau")
+    print("2. Vérifier si une adresse IP appartient à un réseau")
+    print("3. Calculer le plan d'adressage")
+    print("4. Quitter")
 
-else:
-    print(ip)
-    print (masque)
+    choix = input("Choisissez une option (1/2/3/4): ")
+
+    if choix == "1":
+        # Demandez à l'utilisateur d'entrer l'adresse IP et le masque
+        ip=ipEntered()
+        masque = input()
+        print ('Voulez-vous une découpe en sous-réseaux ? (oui/non) ')
+        reponse = input()
+
+    if reponse=="oui":
+        print ('Veuillez entrez un masque ')
+        masque = input()
+
+    else:
+        print(ip)
+        print (masque)
+        resultats = calculer_adresses(adresse_ip, masque)
+    elif choix == "2":
+
+    elif choix == "3":
+
+    elif choix == "4":
+        break
+
+    else:
+        print("Option invalide. Choisissez 1, 2, 3 ou 4.")   
 
 
